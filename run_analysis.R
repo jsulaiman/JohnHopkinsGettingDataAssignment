6012a423f8611c0c47e3cc57b0c1fbfd35615268
jsulaima <lai.s.johan@gmail.com>
2016-05-24 02:23:47 -0700

    final commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+# Author: Johan Sulaiman
+
 # 1) a tidy data set as described below
 # 2) a link to a Github repository with your script for performing the analysis
 # 3) a code book that describes the variables, the data, and any transformations or work that you performed 
@@ -9,12 +11,39 @@
 # Appropriately labels the data set with descriptive variable names.
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-#run_analysis <-function() {
 
-  setwd("P:/Personal/Study/Coursera/Cleaning Data/JohnHopkinsGettingDataAssignment")
-  print("Hi",)
-  train=read.table("train/X_train.txt")
-  test=read.table("test/X_test.txt")
-  mergedData=merge(train,test,all=TRUE)
-  print(head(mergedData),)
-#}
+  setwd("C:/Users/lindec25/Johan/Study/Coursera/R/Getting and Cleansing Data")
+  message("Reading Training and Testing data")
+  Xtrain=read.table("train/X_train.txt",sep="",header=FALSE)
+  Xtest=read.table("test/X_test.txt",sep="",header=FALSE)
+  Ytrain=read.table("train/y_train.txt",sep="",header=FALSE)
+  Ytest=read.table("test/y_test.txt",sep="",header=FALSE)
+  SubjectTrain=read.table("train/subject_train.txt",sep="",header=FALSE)
+  SubjectTest=read.table("test/subject_test.txt",sep="",header=FALSE)
+
+  message("Reading features and assign descriptive activity names")
+  features = read.table("features.txt",sep="",header=FALSE)
+  names(Xtrain)=features[,2]
+  names(Xtest)=features[,2]
+  names(Ytrain)="Class_Label"
+  names(Ytest)="Class_Label"
+  names(SubjectTrain)="SubjectID"
+  names(SubjectTest)="SubjectID"
+  
+  message("Merging Training and Test data")
+  XData = rbind(Xtrain,Xtest)
+  YData = rbind(Ytrain,Ytest)
+  SData = rbind(SubjectTrain,SubjectTest)
+  FullData = cbind(XData,YData,SData)
+  
+  message("Extract just the Mean, Standard Deviation, Class Label, SubjectID columns")
+  matchingCols = grep("mean|std|Class_|Subject", names(FullData))
+  SubFullData = FullData[,matchingCols]
+  
+  message("Match Activity Names by Class Label")
+  ActivityNames = read.table("activity_labels.txt",sep="",header=FALSE)
+  names(ActivityNames)=c("Class_Label","ClassName")
+  FinalData = merge(x=SubFullData,y=ActivityNames,by.x="Class_Label",by.y="Class_Label")
+  
+  message("Write tidy data")
+  write.csv(FinalData,file="tidy.txt",row.names = FALSE)
